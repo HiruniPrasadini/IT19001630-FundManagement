25d0de3c5e9342c32cf7096b46d5fe2f66c22f22
HiruniPrasadini <hiruniprasadini567@gmail.com>
2021-05-13 19:47:20 +0530

    First Commit

---
+++ WebContent/funds.jsp
@@ -0,0 +1,52 @@
+<%@page import="com.Fund"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+<link rel="stylesheet" href="Views/bootstrap.min.css">
+<script src="Components/jquery-3.2.1.min.js"></script>
+<script src="Components/Fund.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-6">
+				<h1>Fund Management</h1>
+				<form id="formFund" name="formFund" method="post" action="funds.jsp">
+
+					Name: <input id="fundName" name="fundName" type="text"
+						class="form-control form-control-lg"><br> Research: <input
+						id="fundResearch" name="fundResearch" type="text"
+						class="form-control form-control-lg"><br> Type:<input
+						id="fundType" name="fundType" type="text"
+						class="form-control form-control-lg"><br> Amount: <input
+						id="fundAmount" name="fundAmount" type="text"
+						class="form-control form-control-lg"><br>
+					Description: <input id="fundDesc" name="fundDesc" type="text"
+						class="form-control form-control-lg"> <br> Date: <input
+						id="fundDate" name="fundDate" type="text"
+						class="form-control form-control-lg"><br> <input
+						id="btnSave" name="btnSave" type="button" value="Save"
+						class="btn btn-primary"> <input type="hidden"
+						id="hidFundIDSave" name="hidFundIDSave" value="">
+
+				</form>
+
+				<div id="alertSuccess" class="alert alert-success"></div>
+				<div id="alertError" class="alert alert-danger"></div>
+				<br>
+				<div id="divItemsGrid">
+					<%
+						Fund fundObj = new Fund();
+					out.print(fundObj.readFunds());
+					%>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ src/com/FundsAPI.java
@@ -0,0 +1,103 @@
+
+package com;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import com.Fund;
+/**
+ * Servlet implementation class FundsAPI
+ */
+@WebServlet("/FundsAPI")
+public class FundsAPI extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	Fund fundObj = new Fund();
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public FundsAPI() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String output = fundObj.insertFund(request.getParameter("fundName"), request.getParameter("fundResearch"),
+				request.getParameter("fundType"), request.getParameter("fundAmount"),
+				request.getParameter("fundDesc"), request.getParameter("fundDate"));
+		
+		response.getWriter().write(output);
+	}
+
+	
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			 throws ServletException, IOException
+			{
+		 Map paras = getParasMap(request);
+		 String output = fundObj.updateFund(paras.get("hidFundIDSave").toString(),
+		 paras.get("funderName").toString(),
+		 paras.get("fundResearch").toString(),
+		paras.get("paymentType").toString(),
+		paras.get("fundAmount").toString(),
+		paras.get("fundDesc").toString(),
+		paras.get("fundDate").toString());
+		response.getWriter().write(output); 
+			} 
+	
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			 throws ServletException, IOException
+			{
+			 Map paras = getParasMap(request);
+			 String output = fundObj.deleteItem(paras.get("fundID").toString());
+			response.getWriter().write(output);
+			}
+	
+	// Convert request parameters to a Map
+	private static Map getParasMap(HttpServletRequest request)
+	{
+	     Map<String, String> map = new HashMap<String, String>();
+	     
+	try
+	 {
+	     Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+	     String queryString = scanner.hasNext() ? scanner.useDelimiter("\\A").next() : "";
+	     scanner.close();
+	     
+	    String[] params = queryString.split("&");
+	 for (String param : params)
+	 { 
+
+	     String[] p = param.split("=");
+	    map.put(p[0], p[1]);
+	 }
+	 }
+	catch (Exception e)
+	 {
+	 }
+	return map;
+	 }
+
+
+}
